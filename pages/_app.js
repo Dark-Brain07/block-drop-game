2b6b465940718d14984aec9e05a5105026eb9983
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2025-10-05 15:04:13 +0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import { Web3Provider } from '../contexts/Web3Context';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Web3Provider>
+      <Component {...pageProps} />
+    </Web3Provider>
+  );
+}
